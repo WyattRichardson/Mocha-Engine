e996aa6da20dcab6ea6b555fc96686d50d497b55
Richardson <richarwy@amazon.com>
2023-03-15 13:23:45 -0400

    Changed Mando Tex

--- src/test/java/richardson/wyatt/Main.java
+++ src/test/java/richardson/wyatt/Main.java
@@ -14,7 +14,6 @@ import richardson.wyatt.game_entities.entity.EntityComponent.Type;
 import richardson.wyatt.game_entities.lighting.Light;
 import richardson.wyatt.game_entities.model.Model;
 import richardson.wyatt.game_entities.textures.ModelTexture;
-import richardson.wyatt.game_entities.textures.Texture;
 import richardson.wyatt.utils.KeyInput;
 
 import static org.lwjgl.glfw.GLFW.*;
@@ -56,7 +55,7 @@ public class Main {
 			}
 			
 		};
-		ModelTexture mandoTex = new ModelTexture("silvertex.png", GL_TEXTURE0);
+		ModelTexture mandoTex = new ModelTexture("metal.png", GL_TEXTURE0);
 		mandoModel.setTexture(mandoTex);
 		mando.addComponent(mandoModel);
 		mando.addComponent(mandoTransform);
@@ -67,13 +66,13 @@ public class Main {
 		
 
 		Entity sun = new Light("Sun", new Vector3f(1f,1f,1f));
-		EntityComponent sunTransform = new Transform(Type.TRANSFORM, 0, 200, 0, 0, 0, 0, 1);
+		EntityComponent sunTransform = new Transform(Type.TRANSFORM, 0, 200, 200, 0, 0, 0, 1);
 		EntityComponent sunController = new EntityController(Type.CONTROLLER) {
 
 			@Override
 			public void tick(float dt) {
 				Transform transform = (Transform) sunTransform;
-				float speed = 100;
+				float speed = 1000;
 				if(KeyInput.isKeyDown(GLFW_KEY_LEFT)){
 					transform.getPosition().x -= (speed * dt);
 				}
@@ -81,10 +80,10 @@ public class Main {
 					transform.getPosition().x += (speed * dt);
 				}
 				if(KeyInput.isKeyDown(GLFW_KEY_UP)) {
-					transform.getPosition().z -= (speed * dt);
+					transform.getPosition().y += (speed * dt);
 				}
 				if(KeyInput.isKeyDown(GLFW_KEY_DOWN)) {
-					transform.getPosition().z += (speed * dt);
+					transform.getPosition().y -= (speed * dt);
 				}
 			}
 
